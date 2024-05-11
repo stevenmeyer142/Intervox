b02536a4da948a5f934844698f86015e77fc0960
Steve Meyer <stevenmeyer142@gmail.copm>
2024-05-11 13:19:38 -0400

    upgrading to conan 2

--- src/main/native/conanfile.py
+++ src/main/native/conanfile.py
@@ -1,15 +1,15 @@
-from conans import ConanFile, CMake
+from conan import ConanFile
 
 class IntervoxConan(ConanFile):
-    generators = "cmake", "xcode"
+    generators = "CMakeToolchain", "XcodeToolchain"
     settings = "os", "compiler", "build_type", "arch"
     requires =  "glm/0.9.9.8"
-    default_options = {"moltenvk:shared": True}
+    default_options = {"moltenvk/*:shared": True}
 
 
     def requirements(self):
         if self.settings.os == "Macos":
-            self.requires("moltenvk/1.1.10")
+            self.requires("moltenvk/1.2.2")
         else:
             self.requires("vulkan-loader/1.2.190")
 
